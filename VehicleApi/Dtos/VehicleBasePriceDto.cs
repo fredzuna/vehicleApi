91f4c887cb17434663c644a71ae6f28eaa27f7bb
Fredy <fredzuna@hotmail.com>
2024-08-17 00:07:58 -0400

    validate input field

--- VehicleApi/Dtos/VehicleBasePriceDto.cs
+++ VehicleApi/Dtos/VehicleBasePriceDto.cs
@@ -1,10 +1,15 @@
 using VehicleApi.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace VehicleApi.Dtos
 {
     public class VehicleBasePriceDto
     {
+        [Required]
         public VehicleTypeEnum Type { get; set; }
-        public decimal BasePrice { get; set; }        
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "The base price must be greater than 0.")]
+        public decimal BasePrice { get; set; }
     }
 }
